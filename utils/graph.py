3def7837ea743b2754731812442601193f6ffda1
benf549 <benjaminpfry@gmail.com>
2025-05-22 12:19:19 -0400

    Fix bug where distance between donor and acceptor was between donor hydrogen and acceptor.

--- utils/graph.py
+++ utils/graph.py
@@ -51,28 +51,34 @@ class PolarAtomGraph:
         atom_neighbors = list(self.graph.neighbors(atom_index))
 
         neighbor_ligand_atom_indices = sorted([
-            (x, self.distance_matrix[atom_index, x]) for x in atom_neighbors if x in self.ligand_atom_indices
+            (x, self.distance_matrix[atom_index, x]) 
+            for x in atom_neighbors if x in self.ligand_atom_indices
         ], key=lambda x: x[1])
+
         neighbor_protein_atom_indices = sorted([
-            (x - len(self.ligand_polar_atoms), self.distance_matrix[atom_index, x]) for x in atom_neighbors if x not in self.ligand_atom_indices
+            (x - len(self.ligand_polar_atoms), self.distance_matrix[atom_index, x]) 
+            for x in atom_neighbors if x not in self.ligand_atom_indices
         ], key=lambda x: x[1])
 
         return [x[0] for x in neighbor_ligand_atom_indices], [x[0] for x in neighbor_protein_atom_indices]
     
-    def _consume_neighborhood(self, curr_polar_atom, neighbor_polar_atoms):
+    def _consume_neighborhood(self, curr_polar_atom, neighbor_polar_atoms, debug=False):
         found_valid_hbond = False
         for neighbor_polar_atom in neighbor_polar_atoms:
             # Check the current atom and neighbor atom for donor hydrogens.
             for hydrogen in curr_polar_atom.donor_hydrogens:
                 found_valid_hbond = (
                     found_valid_hbond or 
-                    is_valid_hbond(curr_polar_atom, hydrogen, neighbor_polar_atom, hydrogen_clash_check=self.run_hydrogen_atom_clash_check)
+                    is_valid_hbond(curr_polar_atom, hydrogen, neighbor_polar_atom, hydrogen_clash_check=self.run_hydrogen_atom_clash_check, debug=debug)
                 )
             for neighbor_hydrogen in neighbor_polar_atom.donor_hydrogens:
                 found_valid_hbond = (
                     found_valid_hbond or 
-                    is_valid_hbond(neighbor_polar_atom, neighbor_hydrogen, curr_polar_atom, hydrogen_clash_check=self.run_hydrogen_atom_clash_check)
+                    is_valid_hbond(neighbor_polar_atom, neighbor_hydrogen, curr_polar_atom, hydrogen_clash_check=self.run_hydrogen_atom_clash_check, debug=debug)
                 )
+            if debug: print(curr_polar_atom.name, curr_polar_atom.parent_group_identifier, neighbor_polar_atom.name, neighbor_polar_atom.parent_group_identifier, found_valid_hbond)
+            if found_valid_hbond:
+                break
         return found_valid_hbond
     
     def compute_ligand_buns(self):
@@ -119,67 +125,77 @@ class PolarAtomGraph:
 
 
 def is_valid_hbond(
-    donor_atom: PolarAtom, donor_hydrogen: DonorHydrogen, acceptor: PolarAtom, 
+    donor_atom: PolarAtom, donor_hydrogen: DonorHydrogen, acceptor_atom: PolarAtom, 
     hydrogen_clash_check: bool, debug: bool = False
 ):
-    # Check that donor and acceptor are possible.
+    """
+    Check if a hydrogen bond is valid based on the distance and angle between the donor and acceptor atoms.
+
+    The function checks the following conditions:
+    1. The donor and acceptor atoms are of compatible types (N, O, S).
+    2. The donor hydrogen is not already engaged in another bond.
+    3. The donor and acceptor atoms have the capacity remaining (unbonded lone pairs) to form a hydrogen bond.
+    4. The distance between the donor and acceptor atoms is within the cutoff distance.
+    5. The angle between the donor-hydrogen and acceptor-hydrogen vectors is greater than the minimum angle.
+    6. The donor hydrogen does not clash with any hydrogens on the acceptor atom.
+
+    If all conditions are met, the function returns True, indicating a valid hydrogen bond. Otherwise, it returns False.
+    """
 
     threshold_distance = None
-    if (donor_atom.element in ('N', 'O') and acceptor.element in ('N', 'O')):
+    if (donor_atom.element in ('N', 'O') and acceptor_atom.element in ('N', 'O')):
         threshold_distance = ON_ON_HYDROGEN_BOND_DISTANCE_CUTOFF
     elif (
-        (donor_atom.element == 'S' and acceptor.element in ('N', 'O')) or
-        (donor_atom.element in ('N', 'O') and acceptor.element == 'S')
+        (donor_atom.element == 'S' and acceptor_atom.element in ('N', 'O')) or
+        (donor_atom.element in ('N', 'O') and acceptor_atom.element == 'S')
     ):
         threshold_distance = ON_S_HYDROGEN_BOND_DISTANCE_CUTOFF
-    elif (donor_atom.element == 'S' and acceptor.element == 'S'):
+    elif (donor_atom.element == 'S' and acceptor_atom.element == 'S'):
         threshold_distance = S_TO_S_HYDROGEN_BOND_DISTANCE_CUTOFF
     else:
-        raise NotImplementedError(f'Unsupported donor-acceptor pair: {donor_atom.element}, {acceptor.element}')
+        raise NotImplementedError(f'Unsupported donor-acceptor pair: {donor_atom.element}, {acceptor_atom.element}')
 
     # Check that donor hydrogen is not already doing something else.
     if donor_hydrogen.is_engaged:
          # It's possible that this hbond has already been detected in the other direction so check for that.
-        if donor_hydrogen.engaged_to is acceptor:
-            if debug: print(donor_atom.name, donor_hydrogen.name, acceptor.name, True)
+        if donor_hydrogen.engaged_to is acceptor_atom:
+            if debug: print(donor_atom.name, donor_hydrogen.name, acceptor_atom.name, True, 'already engaged to acceptor')
             return True
 
         # If the donor hydrogen is already engaged to something else, we can't use it for this hbond.
-        # if debug: print(donor_atom.name, donor_hydrogen.name, acceptor.name, False, 'donor hydrogen already engaged')
+        if debug: print(donor_atom.name, donor_hydrogen.name, acceptor.name, False, 'donor hydrogen already engaged')
         return False
     
     # Check if the donor and acceptor have capacity for hbonding.
-    if not ((donor_atom.donor_count > 0) and (acceptor.acceptor_count > 0)):
-        if debug: print(donor_atom.name, donor_hydrogen.name, acceptor.name, False, 'not enough donors or acceptors')
+    if not ((donor_atom.donor_count > 0) and (acceptor_atom.acceptor_count > 0)):
+        if debug: print(donor_atom.name, donor_hydrogen.name, acceptor_atom.name, False, 'not enough donors or acceptors')
         return False
 
     # Check if the donor-acceptor distance is within the cutoff
-    distance = np.linalg.norm(donor_hydrogen.coord - acceptor.coord)
+    distance = np.linalg.norm(donor_atom.coord - acceptor_atom.coord)
     if (distance > threshold_distance) or (distance < MIN_HBOND_DISTANCE):
-        if debug: print(donor_atom.name, donor_hydrogen.name, acceptor.name, False, f'distance too far {distance}')
+        if debug: print(donor_atom.name, donor_hydrogen.name, acceptor_atom.name, False, f'distance too far {distance}')
         return False
 
     # Angle between D->H and A->H vectors should be greater than MIN_HBOND_ANGLE
     angle = np.rad2deg(np.arccos((
-        np.dot(donor_hydrogen.coord - donor_atom.coord, donor_hydrogen.coord - acceptor.coord) /
-        (np.linalg.norm(donor_hydrogen.coord - donor_atom.coord) * np.linalg.norm(donor_hydrogen.coord - acceptor.coord))),
+        np.dot(donor_hydrogen.coord - donor_atom.coord, donor_hydrogen.coord - acceptor_atom.coord) /
+        (np.linalg.norm(donor_hydrogen.coord - donor_atom.coord) * np.linalg.norm(donor_hydrogen.coord - acceptor_atom.coord))),
     ))
     if angle < MIN_HBOND_ANGLE:
-        if debug: print(donor_atom.name, donor_hydrogen.name, acceptor.name, False, f'angle too small {angle}')
+        if debug: print(donor_atom.name, donor_hydrogen.name, acceptor_atom.name, False, f'angle too small {angle}')
         return False
 
-    # TODO: check that donor hydrogen does not clash with any hydrogens on the acceptor?
+    # Check that donor hydrogen does not clash with hydrogens on the acceptor?
     if hydrogen_clash_check:
-        for acc_donor in acceptor.donor_hydrogens:
+        for acc_donor in acceptor_atom.donor_hydrogens:
             if np.linalg.norm(donor_hydrogen.coord - acc_donor.coord) < H_TO_H_CLASH_DIST:
-                if debug: print(donor_atom.name, donor_hydrogen.name, acceptor.name, False, f'clash with {acc_donor.name}')
+                if debug: print(donor_atom.name, donor_hydrogen.name, acceptor_atom.name, False, f'clash with {acc_donor.name}')
                 return False
 
     # Adjust the donor and acceptor counts for the hydrogen bond
     donor_atom.donor_count -= 1
-    acceptor.acceptor_count -= 1
-    donor_hydrogen.engage(acceptor)
-    if debug: print(donor_atom.name, donor_hydrogen.name, acceptor.name, True)
+    acceptor_atom.acceptor_count -= 1
+    donor_hydrogen.engage(acceptor_atom)
+    if debug: print(donor_atom.name, donor_hydrogen.name, acceptor_atom.name, distance, angle, True)
     return True
-
-
